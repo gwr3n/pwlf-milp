313523520e3e768890e1921adf4da817b67dd1a9
Roberto Rossi <robros@gmail.com>
2023-10-20 21:58:02 +0100

    Updated OPL version

--- src/test/java/milp_model/backorders/TestMILPModelPenalty.java
+++ src/test/java/milp_model/backorders/TestMILPModelPenalty.java
@@ -42,7 +42,7 @@ public class TestMILPModelPenalty {
    /**
     * Environment variable
     * 
-    * DYLD_LIBRARY_PATH=/Applications/CPLEX_Studio128/opl/bin/x86-64_osx/
+    * DYLD_LIBRARY_PATH=/Applications/CPLEX_Studio1210/opl/bin/x86-64_osx/
     * 
     * VM arguments
     * 
